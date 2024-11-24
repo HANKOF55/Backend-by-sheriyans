9bfa8bad608d5d698ad896455d03789d6e8cbfad
HARISH <harishkr0055@gmail.com>
2024-11-24 13:41:13 +0530

    create a server and route using http

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const catMe = require('cat-me');
+const http = require("http");
 
-console.log(catMe());
+
+const server = http.createServer((req, res) => {
+    if(req.url == "/"){
+        res.end("<h1> Hello Harish</h1>")
+    }
+
+    else if(req.url == "/home"){
+        res.end("<h1>This is Home Page.</h1>");
+    }
+
+    else if(req.url == "/contact"){
+        res.end("<h1>This is Contact Page.</h1>");
+    }
+
+    else if(req.url == "/service"){
+        res.end("<h1>This is Services Page.</h1>");
+    }
+
+    else if(req.url == "/about"){
+        res.end("<h1>This is About Page.</h1>");
+    }
+    
+})
+
+
+server.listen(5000);
