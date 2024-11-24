3d329cc0a2aa17eb044e41807781aa4d7985ac87
HARISH <harishkr0055@gmail.com>
2024-11-24 14:42:02 +0530

    understanding middlewares

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+// importing express package
 const express = require("express");
 
+// storing express function in app variable
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
+// setting up ejs 
+app.set("view engine", "ejs");
+
+
+// setting up routes using express
+
+// creating a middleware
+// middleware generally used for sharing error responses
+app.use((req, res, next) => {
+    console.log("This is a middleware");
+
+    return next(); 
+})
+
+// creating a custom middleware for a single route
+app.get("/", (req, res, next) => {
+    const a = 5; 
+    const b = 4;
+    console.log(a+b);
+
+    next();
+
+}
+ , (req,res) => {
+    res.render("index");
 })
 
 app.get("/about", (req,res) => {
@@ -14,5 +39,5 @@ app.get("/profile", (req,res) => {
     res.send("This is Profile Page.");
 })
 
-
+// listeining server on port 5000
 app.listen(5000)
